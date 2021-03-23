0863b184a2d318fdca9248e2330ad18c4082ec45
Seyma Citak <citak.seymaa@gmail.com>
2021-03-23 22:46:59 +0300

    library class added adn sql database connect

--- lib.py
+++ lib.py
@@ -12,3 +12,23 @@ class Book():
     def __str__(self):
         return "Book Name: {}\nWriter: {}\nPublisher: {}\Kind: {}\nPrinting: {}\n".format(self.name,self.writer,self.publisher,self.kind,self.printing)
 
+
+class Library():
+
+    def __init__(self):
+        self.make_connect()
+
+    def make_connect(self):
+
+        self.connect = sqlite3.connect("library.sqlite3")
+
+        self.cursor = self.connect.cursor()
+
+        query = "CREATE TABLE IF NOT EXISTS bookcase (Name, Writer, Publisher, Kind, Printing)"
+
+        self.cursor.execute(query)
+
+        self.connect.commit()
+
+    def cut_connect(self):
+        self.connect.close()
